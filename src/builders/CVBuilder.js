9d8d9013e2f75da86691c6d29576e4919d31145c
ADEP-123 <andep210399@gmail.com>
2024-10-05 10:01:19 -0500

    feat: :sparkles: creando la clase CVBuilder que contiene los metodos generales de las diferentes formas especificas de generar el CV

---
+++ src/builders/CVBuilder.js
@@ -0,0 +1,31 @@
+class CVBuilder {
+    setInformacionPersonal(informacionPersonal) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    addExperienciaLaboral(experiencia) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    addEducacion(educacion) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    addHabilidades(habilidades) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    addProyectos(proyectos) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    addReferencias(referencias) {
+        throw new Error("Este método debe ser implementado");
+    }
+
+    getCV() {
+        throw new Error("Este método debe ser implementado");
+    }
+}
+
+export default CVBuilder;
