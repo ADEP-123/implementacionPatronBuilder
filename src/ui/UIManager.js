a81afc2204c16e60c65689640119351ab340766c
ADEP-123 <andep210399@gmail.com>
2024-10-05 10:41:37 -0500

    feat: :sparkles: modificacion de la interfaz para que se ajuste al objeto de datos enviado desde el app, eliminacion del index.js porque ya no es necesario

--- src/ui/UIManager.js
+++ src/ui/UIManager.js
@@ -4,57 +4,111 @@ import CVBuilderFuncional from '../builders/CVBuilderFuncional.js';
 
 class UIManager {
     constructor() {
-        this.datosCV = {
-            informacionPersonal: {},
-            experienciaLaboral: [],
-            educacion: [],
-            habilidades: [],
-            proyectos: [],
-            referencias: []
-        };
+        this.builder = null;
     }
 
     elegirFormato(formato) {
-        switch (formato) {
-            case 'cronologico':
-                this.builder = new CVBuilderCronologico();
-                break;
-            case 'funcional':
-                this.builder = new CVBuilderFuncional();
-                break;
-            default:
-                throw new Error("Formato no soportado");
+        if (formato === 'cronologico') {
+            this.builder = new CVBuilderCronologico();
+        } else if (formato === 'funcional') {
+            this.builder = new CVBuilderFuncional();
         }
     }
 
-    agregarInformacionPersonal(datos) {
-        this.datosCV.informacionPersonal = datos;
+    // Recoger la experiencia laboral del formulario
+    recogerExperienciaLaboral() {
+        const experiencias = [];
+        const experienciaLaboralElements = document.querySelectorAll('.experienciaLaboral');
+
+        experienciaLaboralElements.forEach((expElement) => {
+            const titulo = expElement.children[0].value;
+            const empresa = expElement.children[1].value;
+            const fechaInicio = expElement.children[2].value;
+            const fechaFin = expElement.children[3].value;
+
+            experiencias.push({ titulo, empresa, fechaInicio, fechaFin });
+        });
+
+        return experiencias;
     }
 
-    agregarExperienciaLaboral(experiencia) {
-        this.datosCV.experienciaLaboral.push(experiencia);
+    // Recoger la educación del formulario
+    recogerEducacion() {
+        const educacion = [];
+        const educacionElements = document.querySelectorAll('.educacion');
+
+        educacionElements.forEach((eduElement) => {
+            const titulo = eduElement.children[0].value;
+            const institucion = eduElement.children[1].value;
+            const fechaInicio = eduElement.children[2].value;
+            const fechaFin = eduElement.children[3].value;
+
+            educacion.push({ titulo, institucion, fechaInicio, fechaFin });
+        });
+
+        return educacion;
     }
 
-    agregarEducacion(educacion) {
-        this.datosCV.educacion.push(educacion);
+    // Recoger los proyectos del formulario
+    recogerProyectos() {
+        const proyectos = [];
+        const proyectoElements = document.querySelectorAll('.proyecto');
+
+        proyectoElements.forEach((proElement) => {
+            const nombre = proElement.children[0].value;
+            proyectos.push({ nombre });
+        });
+
+        return proyectos;
     }
 
-    agregarHabilidades(habilidades) {
-        this.datosCV.habilidades = habilidades;
+    // Recoger las referencias del formulario
+    recogerReferencias() {
+        const referencias = [];
+        const referenciaElements = document.querySelectorAll('.referencia');
+
+        referenciaElements.forEach((refElement) => {
+            const nombre = refElement.children[0].value;
+            referencias.push({ nombre });
+        });
+
+        return referencias;
     }
 
-    agregarProyectos(proyectos) {
-        this.datosCV.proyectos = proyectos;
-    }
+    // Generar el CV usando el builder seleccionado
+    generarCV(datos) {
+        // Verificar si se ha seleccionado un builder
+        if (!this.builder) {
+            throw new Error("No se ha seleccionado un formato de CV.");
+        }
 
-    agregarReferencias(referencias) {
-        this.datosCV.referencias = referencias;
-    }
-
-    generarCV() {
         const generador = new GeneradorCV(this.builder);
-        return generador.construirCV(this.datosCV);
+
+        // Añadir las diferentes secciones al CV
+        this.builder.setInformacionPersonal(datos.informacionPersonal); 
+
+        datos.experienciaLaboral.forEach(exp => {
+            this.builder.addExperienciaLaboral(exp); 
+        });
+
+        datos.educacion.forEach(edu => {
+            this.builder.addEducacion(edu); 
+        });
+
+        this.builder.addHabilidades(datos.habilidades); 
+        datos.proyectos.forEach(pro => {
+            this.builder.addProyectos(pro); 
+        });
+
+        datos.referencias.forEach(ref => {
+            this.builder.addReferencias(ref); 
+        });
+
+        // Generar y retornar el CV final
+        return generador.construirCV(datos);
     }
+
+
 }
 
 export default UIManager;
